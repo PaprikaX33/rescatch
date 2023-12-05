9a07023acbc400176d9c69e9937b035c663e393b
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2023-12-05 20:36:36 +0800

    initial command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+struct Cmd {
+    port: u32,
+}
+
+fn main() -> std::process::ExitCode {
+    let mut config: Cmd = Cmd { port: 7999 };
+    let mut itr = std::env::args();
+    let _ = itr.next();
+    while let Some(args) = itr.next() {
+        match args.as_str() {
+            "-h" | "--help" => {
+                println!("./program [OPTIONS] [port]");
+                println!("   -h -? --help : help");
+                println!("  Port defaults to 7999");
+                return std::process::ExitCode::from(0);
+            }
+            argm => {
+                config.port = match argm.parse() {
+                    Ok(x) => x,
+                    Err(x) => {
+                        println!("{:?}", x);
+                        return std::process::ExitCode::from(20);
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", config.port);
+    std::process::ExitCode::from(0)
 }
