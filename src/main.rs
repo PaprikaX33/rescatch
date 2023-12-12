ad0a34b6efcdb6a66ffd84dc2210a80a8ac63963
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2023-12-12 18:45:13 +0800

    added the tcp reading from the example in the rust book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+//use std::net::TcpListener;
+use std::io::BufRead;
 struct Cmd {
     port: u32,
 }
@@ -25,6 +27,34 @@ fn main() -> std::process::ExitCode {
             }
         }
     }
-    println!("{}", config.port);
-    std::process::ExitCode::from(0)
+    println!("Starting listeing for connection in port {}", config.port);
+    std::process::ExitCode::from(start_server(config))
+}
+
+fn start_server(conf: Cmd) -> u8 {
+    let listener =
+        std::net::TcpListener::bind(format!("127.0.0.1:{}", conf.port).as_str()).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        match connection_handler(stream) {
+            Ok(_) => (),
+            Err(x) => {
+                println!("{:?}", x);
+            }
+        }
+    }
+    0
+}
+
+fn connection_handler(mut stream: std::net::TcpStream) -> Result<(), std::io::Error> {
+    let buf_reader = std::io::BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
+    Ok(())
 }
