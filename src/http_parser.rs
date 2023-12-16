ce392ffabad50e69cd59b53091c5cad27eed8ead
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2023-12-16 23:18:41 +0800

    moved the http error to the dedicated error module

--- src/http_parser.rs
+++ src/http_parser.rs
@@ -55,20 +55,3 @@ impl FromStr for HttpRequestHeader {
     }
 }
 */
-
-pub struct HttpRequestError(String);
-impl std::error::Error for HttpRequestError {}
-
-impl std::fmt::Display for HttpRequestError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "Parsing HTTP error. Malformed http header: {}", self)
-    }
-}
-
-impl std::fmt::Debug for HttpRequestError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Http Request Error:: ")
-            .field("Malformed header", self)
-            .finish()
-    }
-}
