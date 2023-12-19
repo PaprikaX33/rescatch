e905efe316731896614f88da9f13c1e9f91b7262
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2023-12-19 19:03:52 +0800

    moved the server.rs to multiple submodules

---
+++ src/error/mod.rs
@@ -0,0 +1,4 @@
+pub use http_request::HttpRequestError;
+pub use server::ServerHandlerError;
+mod http_request;
+mod server;
--- src/error/server.rs
+++ src/error/server.rs
@@ -1,26 +1,4 @@
-pub struct HttpRequestError {
-    msg: String,
-}
-impl std::error::Error for HttpRequestError {}
-
-impl std::fmt::Display for HttpRequestError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "Parsing HTTP error. Malformed http header: {}", self.msg)
-    }
-}
-impl From<String> for HttpRequestError {
-    fn from(err: String) -> Self {
-        HttpRequestError { msg: err }
-    }
-}
-impl std::fmt::Debug for HttpRequestError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Http Request Error:: ")
-            .field("Malformed header", &self.msg)
-            .finish()
-    }
-}
-
+use super::HttpRequestError;
 pub enum ServerHandlerError {
     IoErr(std::io::Error),
     HttpRequestError(HttpRequestError),
