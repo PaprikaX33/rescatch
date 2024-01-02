dd9515c00adf891375716abeace4a7ab829e0f5a
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2024-01-03 04:41:00 +0800

    added the more granular http method

--- src/parser/header_line.rs
+++ src/parser/header_line.rs
@@ -1,5 +1,12 @@
+pub enum HttpMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+}
+
 pub struct HttpHeaderLine {
-    method: String,
+    method: HttpMethod,
     uri: String,
     version: String,
 }
@@ -22,8 +29,33 @@ impl HttpHeaderLine {
     pub fn new() -> Self {
         HttpHeaderLine {
             uri: String::new(),
-            method: String::new(),
+            method: HttpMethod::new(),
             version: String::new(),
         }
+    }
+}
+
+impl HttpMethod {
+    fn new() -> Self {
+        return HttpMethod::GET;
+    }
+}
+impl std::fmt::Debug for HttpMethod {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        std::fmt::Display::fmt(&self, f)
+    }
+}
+impl std::fmt::Display for HttpMethod {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match &self {
+                HttpMethod::GET => "GET",
+                HttpMethod::POST => "POST",
+                HttpMethod::PUT => "PUT",
+                HttpMethod::DELETE => "DELETE",
+            }
+        )
     }
 }
